2b7e82a2f582283c33afb57b598892d39b72dacb
MaxAntes <59790700+MaxAntes@users.noreply.github.com>
2021-01-26 23:40:00 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+import telebot
+
+TOKEN = 'TOKEN'
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'go'])
+def start_handler(message):
+    bot.send_message(message.chat.id, 'Привет.')
+
+
+@bot.message_handler(content_types=['text'])
+def text_handler(message):
+    text = message.text.lower()
+    chat_id = message.chat.id
+    if text == "привет":
+        bot.send_message(chat_id, 'Привет, я бот.')
+    elif text == "как дела?":
+        bot.send_message(chat_id, 'Хорошо, а у тебя?')
+    else:
+        bot.send_message(chat_id, 'Простите, я вас не понял :(')
+
+
+bot.polling()
